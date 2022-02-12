813e8994af5d678f19e4135c2746eef29ff1f505
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2022-02-12 21:02:39 +0530

    Delete simple_kafka directory

--- simple_kafka/consumer.py
+++
@@ -1,5 +0,0 @@
-from kafka import KafkaConsumer
-consumer = KafkaConsumer('first_topic', bootstrap_servers='localhost:9092')
-print("Consumer running---->")
-for message in consumer:
-    print(message)
--- simple_kafka/producer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
-print("Producer running---->")
-for i in range(1000):
-    producer.send('first_topic', key=str.encode('key_{}'.format(i)), value=b'some_message_bytes')
-    print("Message sent! ---->")
