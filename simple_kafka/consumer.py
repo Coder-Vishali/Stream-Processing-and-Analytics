cca6492ef27e5f7f8c5ffd6ee6bf6400b75f0054
Vishali Srinivasan <60731083+Coder-Vishali@users.noreply.github.com>
2022-02-12 19:26:29 +0530

    Create consumer.py

---
+++ simple_kafka/consumer.py
@@ -0,0 +1,5 @@
+from kafka import KafkaConsumer
+consumer = KafkaConsumer('first_topic', bootstrap_servers='localhost:9092')
+print("Consumer running---->")
+for message in consumer:
+    print(message)
